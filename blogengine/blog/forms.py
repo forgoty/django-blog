c9bab32f60e52d040318cf1bcb30123f972c690e
Nikita Alkhovik <forgoty13@gmail.com>
2018-11-27 21:48:44 +0300

    Add some changes

--- blogengine/blog/forms.py
+++ blogengine/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Tag
+from .models import Tag, Post
 
 class TagForm(forms.ModelForm):
 
@@ -21,4 +21,27 @@ class TagForm(forms.ModelForm):
 
         if Tag.objects.filter(slug__iexact=new_slug).count():
             raise ValidationError('"{}" already exists'.format(new_slug))
+
+        return new_slug
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'tags']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control'})
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug cannot be named "create"')
+
         return new_slug
